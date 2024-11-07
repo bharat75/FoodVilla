d91b08cd887e96b1003d19f261233d755ae427ef
BharatShubham <bharatshubham75@gmail.com>
2024-11-07 13:18:54 +0530

    Removing console.log

--- src/components/RestrauntCard.js
+++ src/components/RestrauntCard.js
@@ -19,7 +19,6 @@ const RestaurantCard = ({
       style={{ textDecoration: "none", color: "inherit" }}
     >
       <div className="card">
-        {console.log(IMG_CDN_URL + cloudinaryImageId)}
         <img src={IMG_CDN_URL + cloudinaryImageId} />
         <h3>{name}</h3>
         <h5>{cuisines.join(", ")}</h5>
